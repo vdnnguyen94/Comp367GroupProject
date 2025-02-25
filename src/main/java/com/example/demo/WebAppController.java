1eb4a4fae0f4d779d19b0707980b6e9f96508c77
Van Nguyen <vdnnguyen94@gmail.com>
2025-02-24 20:33:34 -0500

    Greeting Features,Controller and index.html

--- src/main/java/com/example/demo/WebAppController.java
+++ src/main/java/com/example/demo/WebAppController.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebAppController {
 	
 	@GetMapping("/")
-	public String Index() {
-		return"index";	
-	}
+	public String Index(Model model) {
+        LocalTime now = LocalTime.now();
+        String greeting;
+
+        if (now.getHour() < 12) {
+            greeting = "Good morning, Van Nguyen";
+        } else if (now.getHour() < 18) {
+            greeting = "Good afternoon, Van Nguyen";
+        } else {
+            greeting = "Good evening, Van Nguyen";
+        }
+
+        model.addAttribute("greetingMessage", greeting);
+        return "index"; 
+    }
+		
 }
