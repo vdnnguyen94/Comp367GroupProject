bb3ea30da947409e3efb98d9aaf6515d27dd9e69
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-11 02:11:30 -0400

    fix(#19) fix stage in SpringBootApplication

--- src/main/java/com/example/demo/Comp367Project.java
+++ src/main/java/com/example/demo/Comp367Project.java
@@ -10,6 +10,8 @@ public class Comp367Project {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
 		String port = dotenv.get("APP_PORT", "5173");
+		String stage = dotenv.get("STAGE", "NOT LOADED");
+		System.out.println("Current Stage: " + stage);
 		System.setProperty("server.port", port);
 		SpringApplication.run(Comp367Project.class, args);
 		System.out.println("Spring Boot WEB running on PORT " + port);
